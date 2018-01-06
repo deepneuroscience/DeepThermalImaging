86efbb3dd7337934a040970cad0bba08107d337c
Deep Neuroscience <youngjun.j.cho@gmail.com>
2018-01-06 20:26:05 +0000

    Create KelvinImageMode.java
    This is an example code

---
+++ example code for FLIR One sdk/KelvinImageMode.java
@@ -0,0 +1,2 @@
+                RenderedImage.ImageType imageType = RenderedImage.ImageType.ThermalRadiometricKelvinImage; // To set "KelvinImage mode"
+                frameProcessor.setImageTypes(EnumSet.of(imageType)); // To initiate the frameProcess instance
